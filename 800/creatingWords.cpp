87c32a53fa05dab96d5de7bd772f940c69212a94
RavalTejas <tejaskumarraval.cse21@aii.ac.in>
2024-06-15 14:30:08 +0530

    Problem added in 800 segement

---
+++ 800/creatingWords.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+using ll = long long int;
+//using in = for(auto& v:a)cin>>a;
+#define push_back pb;
+
+void solve(){
+    string a,b;
+    cin>>a>>b;
+    cout<<b[0]<<a[1]<<a[2]<<" "<<a[0]<<b[1]<<b[2]<<endl;
+}
+int main(){
+ ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+int n;
+cin>>n;
+ for(int i = 0;i<n;i++){
+solve();
+}}
